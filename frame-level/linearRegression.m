201002baf80344dc6087b5243ab3f32c8c914a7f
Jonathan Avila <jonathan.edav@gmail.com>
2021-03-23 10:31:17 -0600

    Add prediction on dev set

--- frame-level/linearRegression.m
+++ frame-level/linearRegression.m
@@ -1,27 +1,28 @@
-% Linear regression
+% Linear regression model
 
 % add necessary files to path
 addpath(genpath('midlevel-master'));
 addpath(genpath('calls'));
 
-directory = 'C:\Users\nullv\OneDrive\Documents\GitHub\dissatisfaction-models\';
+dirWorking = append(pwd, "\");
 
 % get feature spec (mono.fss)
-featureSpec = getfeaturespec(append(directory, "mono.fss"));
-
+featureSpec = getfeaturespec(append(dirWorking, "mono.fss"));
 
 % get the track lists
-trackListTrain = gettracklist(append(directory, "frame-level\train.tl"));
-trackListDev = gettracklist(append(directory, "frame-level\dev.tl"));
+trackListTrain = gettracklist(append(dirWorking, "frame-level\train.tl"));
+trackListDev = gettracklist(append(dirWorking, "frame-level\dev.tl"));
 
 % get X (monster regions) and Y (labels)
-[Xtrain, yTrain] = getXYfromTrackList(trackListTrain, directory, featureSpec);
-[Xdev, yDev] = getXYfromTrackList(trackListDev, directory, featureSpec);
+[Xtrain, yTrain] = getXYfromTrackList(trackListTrain, dirWorking, featureSpec);
+[Xdev, yDev] = getXYfromTrackList(trackListDev, dirWorking, featureSpec);
 
 %%
-linearModel = fitlm(Xtrain, yTrain);
-
-coefficients = linearModel.Coefficients.Estimate;
+% train
+model = fitlm(Xtrain, yTrain);
+%%
+% print coefficient info
+coefficients = model.Coefficients.Estimate;
 
 [coefficientSorted, coeffSortedIdx] = sort(coefficients, 'descend');
 nCoeffPrint = 5;
@@ -31,3 +32,10 @@ for nCoeff = 1:nCoeffPrint
     coeffValue = coefficientSorted(nCoeff);
     fprintf('\tCoefficient %d with value %f\n', coeff, coeffValue);
 end
+
+% predict on the dev set
+yPred = predict(model, Xdev);
+
+% calculate mean absolute error
+mae = mean(abs(yPred - yDev));
+disp(['Mean Absolute Error = ', num2str(mae)]);
