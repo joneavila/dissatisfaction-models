886d3f89928329938cf417511b03b89715f5cea7
Jonathan Avila <jonathan.edav@gmail.com>
2021-03-29 10:22:39 -0600

    Add normalization

--- frame-level/generateHistograms.m
+++ frame-level/generateHistograms.m
@@ -1,23 +1,15 @@
-% generateHistograms.m - save a histogram of each feature, use the train and dev set
+% generateHistograms.m Save a histogram of each feature in feature spec. 
+% Use the train and dev set.
 
-% add necessary files to path
-addpath(genpath('midlevel-master'));
-addpath(genpath('calls'));
+featureSpec = getfeaturespec('.\mono.fss');
 
-dirWorking = append(pwd, "\");
-
-% get feature spec (mono.fss)
-featureSpec = getfeaturespec('mono.fss');
-
-% get the track lists
-trackListTrain = gettracklist("train.tl");
-trackListDev = gettracklist("dev.tl");
+trackListTrain = gettracklist(".\frame-level\train.tl");
+trackListDev = gettracklist(".\frame-level\dev.tl");
 
 % get X (monster regions) and Y (labels)
-[Xtrain, yTrain] = getXYfromTrackList(trackListTrain, dirWorking, featureSpec);
-[Xdev, yDev] = getXYfromTrackList(trackListDev, dirWorking, featureSpec);
+[Xtrain, yTrain] = getXYfromTrackList(trackListTrain, featureSpec);
+[Xdev, yDev] = getXYfromTrackList(trackListDev, featureSpec);
 
-%%
 % 'n' and 'nn' are the negative class (0), 'd' and 'dd' are the positive
 % class (1)
 trainNeutral = Xtrain(yTrain==0, :);
@@ -29,20 +21,22 @@ devDissatisfied = Xdev(yDev==1, :);
 % t-test will compare neutral and dissatisfied
 N = [trainNeutral; devNeutral];
 D = [trainDissatisfied; devDissatisfied];
-%%
-% save a histogram for each feature
+
+%% save a histogram for each feature
 
 % config
 nBins = 32;
-barColorN = '#2196F3';
-barColorD = '#FF9800';
+barColorN = '#1e88e5'; % blue
+barColorD = '#fb8c00'; % orange
 
-imageDir = append(pwd, "\frame-level\images\");
+imageDir = append(pwd, "\frame-level\histograms\");
 status = mkdir(imageDir);
 if ~status
     error("Error creating image directory");
 end
 
+% documentation on plotting mulitple histograms: 
+% https://www.mathworks.com/help/matlab/ref/matlab.graphics.chart.primitive.histogram.html#buiynvy-13
 for featureNum = 1:size(featureSpec, 2)
     
     f = figure('Visible', 'off');
@@ -57,9 +51,17 @@ for featureNum = 1:size(featureSpec, 2)
     hD = histogram(D(:, featureNum), nBins);
     hD.FaceColor = barColorD;
     
+    % normalize the histograms so that all bar heights add to 1
+    hN.Normalization = 'probability';
+    hD.Normalization = 'probability';
+    
+    % adjust bars so that both plots align
+    hN.BinWidth = hD.BinWidth;
+    hN.BinEdges = hD.BinEdges;
+    
     % add titles, axes labels, and legend
     feature = featureSpec(featureNum);
-    titleText = sprintf('feat%02d %s', featureNum, feature.abbrev);
+    titleText = sprintf('feat%02d %s (normalized)', featureNum, feature.abbrev);
     subtitleText = sprintf('train+dev, nBins=%d', nBins);
     title(titleText, subtitleText);
     ylabel('Number in bin');
@@ -73,5 +75,5 @@ for featureNum = 1:size(featureSpec, 2)
     
     clf;
 end
-disp("done");
+disp("Done");
 
