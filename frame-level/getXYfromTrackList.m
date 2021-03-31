de6a2553f4c96912a8e346790734ece6d2737bc8
Jonathan Avila <jonathan.edav@gmail.com>
2021-03-31 13:49:16 -0600

    Add return matchingTrackNums and matchingFrameTimes

--- frame-level/getXYfromTrackList.m
+++ frame-level/getXYfromTrackList.m
@@ -1,19 +1,30 @@
-function [X, y] = getXYfromTrackList(trackList, featureSpec, useAllAnnotators)
-% GETXYFROMTRACKLIST Features are stored in X, labels are stored in y. See
-% GETXYFROMFILE.
+function [X, y, matchingTrackNums, matchingFrameTimes] = ...
+    getXYfromTrackList(trackList, featureSpec, useAllAnnotators)
+% GETXYFROMTRACKLIST Features are stored in X, labels are stored in y. 
+% See also GETXYFROMFILE.
 
     X = [];
     y = [];
+    matchingTrackNums = [];
+    matchingFrameTimes = [];
+    
     nTracks = size(trackList, 2);
     for trackNum = 1:nTracks
+        
         track = trackList{trackNum};
         
-        fprintf('[%d/%d] Getting data for %s\n', trackNum, nTracks, track.filename);
+        fprintf('[%d/%d] Getting X and y for %s\n', trackNum, nTracks, ...
+            track.filename);
+        tic;
+        [dialogX, dialogY, dialogMatchingTimes] = ...
+            getXYfromFile(track.filename, featureSpec, useAllAnnotators);
+        toc;
         
-        [dialogX, dialogY] = getXYfromFile(track.filename, featureSpec, useAllAnnotators);
-        
-        % appending is ugly but isn't too slow here
+        % TODO appending is slow
         X = [X; dialogX];
         y = [y; dialogY];
+        toAppend = ones([size(dialogX, 1) 1]) * trackNum;
+        matchingTrackNums = [matchingTrackNums; toAppend];
+        matchingFrameTimes = [matchingFrameTimes; dialogMatchingTimes];
     end
 end
