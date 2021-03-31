636fe6a99be7e129a91f68d5dc5676be4fb1ba57
Jonathan Avila <jonathan.edav@gmail.com>
2021-03-31 15:55:52 -0600

    Add ignore making clips from frames that are too close to each other

--- frame-level/linearRegression.m
+++ frame-level/linearRegression.m
@@ -1,21 +1,38 @@
 % linearRegression.m Frame-level linear regression model
 
 %% prepare the data
+useTestSet = false;
+
 trackListTrain = gettracklist('.\frame-level\train.tl');
 trackListDev = gettracklist('.\frame-level\dev.tl');
 trackListTest = gettracklist('.\frame-level\test.tl');
 
-% TODO use full spec file
 featureSpec = getfeaturespec('.\mono-simple.fss');
 
 useAllAnnotators = false;
 
-[Xtrain, yTrain, matchingTrackNumsTrain, matchingFrameTimesTrain] = ...
+[Xtrain, yTrain, frameTrackNumsTrain, frameTimesTrain, frameUtterancesTrain] = ...
     getXYfromTrackList(trackListTrain, featureSpec, useAllAnnotators);
-[Xdev, yDev, matchingTrackNumsDev, matchingFrameTimesDev] = ...
+[Xdev, yDev, frameTrackNumsDev, frameTimesDev, frameUtterancesDev] = ...
     getXYfromTrackList(trackListDev, featureSpec, useAllAnnotators);
-% [Xtest, yTest, matchingTrackNumsTest, matchingFrameNumsTest] = ...
-%     getXYfromTrackList(trackListTest, featureSpec, useAllAnnotators);
+[Xtest, yTest, frameTrackNumsTest, frameTimesTest, frameUtterancesTest] = ...
+    getXYfromTrackList(trackListTest, featureSpec, useAllAnnotators);
+
+if useTestSet
+    Xcompare = Xtest;
+    yCompare = yTest;
+    frameTrackNumsCompare = frameTrackNumsTest;
+    frameTimesCompare = frameTimesTest;
+    frameUtterancesCompare = frameUtterancesTest;
+    trackListCompare = trackListTest;
+else
+    Xcompare = Xdev;
+    yCompare = yDev;
+    frameTrackNumsCompare = frameTrackNumsDev;
+    frameTimesCompare = frameTimesDev;
+    frameUtterancesCompare = frameUtterancesDev;
+    trackListCompare = trackListDev;
+end
 %% train regressor
 model = fitlm(Xtrain, yTrain);
 
@@ -36,28 +53,32 @@ end
 fclose(fileID);
 fprintf('Coefficients saved to %s\\%s\n', pwd, outputFilename);
 
-%%  predict on the dev set
-yPred = predict(model, Xdev);
+%%  predict on the compare set
+yPred = predict(model, Xcompare);
 
 % the baseline always predicts dissatisfied (positive class)
-yBaseline = ones([size(Xdev, 1), 1]);
+yBaseline = ones([size(Xcompare, 1), 1]);
 %% print f1 score and more for different thresholds
 thresholdMin = -0.25;
-thresholdMax = 1.55;
+thresholdMax = 1.1;
 thresholdStep = 0.05;
 
 fprintf('min(yPred)=%.3f, max(yPred)=%.3f\n', min(yPred), max(yPred));
 fprintf('thresholdMin=%.2f, thresholdMax=%.2f, thresholdStep=%.2f\n', ...
     thresholdMin, thresholdMax, thresholdStep);
 
-thresholdDev = 0.5;
-yDevLabel = arrayfun(@(x) floatToLabel(x, thresholdDev), yDev, ...
+thresholdCompare = 0.5;
+yCompareLabel = arrayfun(@(x) floatToLabel(x, thresholdCompare), yCompare, ...
     'UniformOutput', false);
 
 nSteps = (thresholdMax - thresholdMin) / thresholdStep;
 threshold = zeros([nSteps 1]);
 precisionLinear = zeros([nSteps 1]);
 precisionBaseline = zeros([nSteps 1]);
+recallLinear = zeros([nSteps 1]);
+recallBaseline = zeros([nSteps 1]);
+scoreLinear = zeros([nSteps 1]);
+scoreBaseline = zeros([nSteps 1]);
 
 thresholdSel = thresholdMin;
 for i = 1:nSteps
@@ -66,49 +87,88 @@ for i = 1:nSteps
         'UniformOutput', false);
     yBaselineLabel = arrayfun(@(x) floatToLabel(x, thresholdSel), ...
         yBaseline, 'UniformOutput', false);
-    [~, precLinear, ~] = fScore(yDevLabel, yPredLabel, 'doomed', ...
-        'successful');
-    [~, precBaseline, ~] = fScore(yDevLabel, yBaselineLabel, 'doomed', ...
-        'successful');
+    [scoLinear, precLinear, recLinear] = fScore(yCompareLabel, ...
+        yPredLabel, 'doomed', 'successful');
+    [scoBaseline, precBaseline, recBaseline] = fScore(yCompareLabel, ...
+        yBaselineLabel, 'doomed', 'successful');
     threshold(i) = thresholdSel;
     precisionLinear(i) = precLinear;
     precisionBaseline(i) = precBaseline;
+    
+    recallLinear(i) = recLinear;
+    recallBaseline(i) = recBaseline;
+    
+    scoreLinear(i) = scoLinear;
+    scoreBaseline(i) = scoBaseline;
+    
+    
     thresholdSel = thresholdSel + thresholdStep;
 end
-disp(table(threshold, precisionLinear, precisionBaseline));
+disp(table(threshold, precisionLinear, precisionBaseline, recallLinear, ...
+    recallBaseline, scoreLinear, scoreBaseline));
 %% failure analysis
 
-yDifference = abs(yDev - yPred);
-[yDifferenceSorted, sortIndex] = sort(yDifference, 'descend');
+% config
+clipSizesSeconds = [1 2];
+numFramesToProbe = 500;
 
-numFramesToClip = 100;
-fprintf('First %d frames with largest difference:\n', numFramesToClip);
+yDifference = abs(yCompare - yPred);
+[yDifferenceSorted, sortIndex] = sort(yDifference, 'descend');
 
 clipDir = sprintf('%s\\clips', pwd);
 [status, msg, msgID] = mkdir(clipDir);
 
-for i = 1:numFramesToClip
-    
-    frameNumDev = sortIndex(i);
-    trackNum = matchingTrackNumsDev(frameNumDev);
-    frameTime = matchingFrameTimesDev(frameNumDev);
-    track = trackListDev{trackNum};
+framesToIgnore = zeros(size(yDifference));
 
-    fprintf('clipFilename=%d.wav frameNumDev=%d time=%s trackNum=%d trackFilename=%s\n', ...
-        i, frameNumDev, char(frameTime), trackNum, track.filename);
+for i = 1:numFramesToProbe
     
+    frameNumCompare = sortIndex(i);
+    
+    % ignore this frame if it has already been included in a clip
+    if framesToIgnore(frameNumCompare)
+        continue;
+    end
+    
+    frameTime = frameTimesCompare(frameNumCompare);
+    trackNum = frameTrackNumsCompare(frameNumCompare);
+    track = trackListCompare{trackNum};
     [audioData, sampleRate] = audioread(track.filename);
     
-    clipSizeSeconds = 1;
-    timeStart = frameTime - seconds(clipSizeSeconds/2);
-    timeEnd = frameTime + seconds(clipSizeSeconds/2);
-    idxStart = round(seconds(timeStart) * sampleRate);
-    idxEnd = round(seconds(timeEnd) * sampleRate);
+    for j = 1:length(clipSizesSeconds)
+        clipSizeSeconds = clipSizesSeconds(j);
+        timeStart = frameTime - seconds(clipSizeSeconds/2);
+        timeEnd = frameTime + seconds(clipSizeSeconds/2);
+        idxStart = round(seconds(timeStart) * sampleRate);
+        idxEnd = round(seconds(timeEnd) * sampleRate);
+        newFilename = sprintf('%s\\clip%d-%dseconds.wav', clipDir, i, j);
+        clipData = audioData(idxStart:idxEnd);
+        audiowrite(newFilename, clipData, sampleRate);
+    end
     
-    newFilename = sprintf('%s\\%d.wav', clipDir, i);
-    clipData = audioData(idxStart:idxEnd);
+    fprintf('clip%d | time=%s | trackFilename=%s\n', ...
+        i, char(frameTime), track.filename);
     
-    audiowrite(newFilename, clipData, sampleRate);
+    % zero out the 
+    % check if any other frame number is within this frame's utterance
+    ignoreSizeSeconds = 1;
+    clipSizeFrames = seconds(ignoreSizeSeconds) / milliseconds(10); % monster frames are 10ms
+    frameNumCompareStart = frameNumCompare - clipSizeFrames / 2;
+    frameNumCompareEnd = frameNumCompare + clipSizeFrames / 2;
+    
+    for frameNumProbe = frameNumCompareStart:frameNumCompareEnd
+        % check if this frame is in the same track as the original
+        if frameTrackNumsCompare(frameNumProbe) ~= frameTrackNumsCompare(frameNumCompare)
+            continue;
+        end
+        
+        % check if this frame is in the same utterance as the original
+        if frameUtterancesCompare(frameNumProbe) ~= frameUtterancesCompare(frameNumCompare)
+            continue;
+        end
+        
+        % if both are true, mark the frame to zero it out
+        framesToIgnore(frameNumProbe) = 1;
+    end
 
 end
 
