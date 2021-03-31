58cf74d2f99314f866a4484cd7d1e30fb5e44f24
Jonathan Avila <jonathan.edav@gmail.com>
2021-03-31 13:49:49 -0600

    Add failure analysis

--- frame-level/linearRegression.m
+++ frame-level/linearRegression.m
@@ -5,16 +5,17 @@ trackListTrain = gettracklist('.\frame-level\train.tl');
 trackListDev = gettracklist('.\frame-level\dev.tl');
 trackListTest = gettracklist('.\frame-level\test.tl');
 
-featureSpec = getfeaturespec('.\mono2.fss'); % TODO use final spec file
+% TODO use full spec file
+featureSpec = getfeaturespec('.\mono-simple.fss');
 
 useAllAnnotators = false;
 
-[Xtrain, yTrain] = getXYfromTrackList(trackListTrain, featureSpec, ...
-    useAllAnnotators);
-[Xdev, yDev] = getXYfromTrackList(trackListDev, featureSpec, ...
-    useAllAnnotators);
-[Xtest, yTest] = getXYfromTrackList(trackListTest, featureSpec, ...
-    useAllAnnotators);
+[Xtrain, yTrain, matchingTrackNumsTrain, matchingFrameTimesTrain] = ...
+    getXYfromTrackList(trackListTrain, featureSpec, useAllAnnotators);
+[Xdev, yDev, matchingTrackNumsDev, matchingFrameTimesDev] = ...
+    getXYfromTrackList(trackListDev, featureSpec, useAllAnnotators);
+% [Xtest, yTest, matchingTrackNumsTest, matchingFrameNumsTest] = ...
+%     getXYfromTrackList(trackListTest, featureSpec, useAllAnnotators);
 %% train regressor
 model = fitlm(Xtrain, yTrain);
 
@@ -46,11 +47,12 @@ thresholdMax = 1.55;
 thresholdStep = 0.05;
 
 fprintf('min(yPred)=%.3f, max(yPred)=%.3f\n', min(yPred), max(yPred));
-fprintf('thresholdMin=%.2f, thresholdMax=%.2f, thresholdStep=%.2f\n', thresholdMin, thresholdMax, thresholdStep);
-fprintf('negative class ("successful") if <= threshold, else positive class ("doomed")\n');
+fprintf('thresholdMin=%.2f, thresholdMax=%.2f, thresholdStep=%.2f\n', ...
+    thresholdMin, thresholdMax, thresholdStep);
 
 thresholdDev = 0.5;
-yDevLabel = arrayfun(@(x) floatToLabel(x, thresholdDev), yDev, 'UniformOutput', false);
+yDevLabel = arrayfun(@(x) floatToLabel(x, thresholdDev), yDev, ...
+    'UniformOutput', false);
 
 nSteps = (thresholdMax - thresholdMin) / thresholdStep;
 threshold = zeros([nSteps 1]);
@@ -60,16 +62,56 @@ precisionBaseline = zeros([nSteps 1]);
 thresholdSel = thresholdMin;
 for i = 1:nSteps
     thresholdSel = round(thresholdSel, 2);
-    yPredLabel = arrayfun(@(x) floatToLabel(x, thresholdSel), yPred, 'UniformOutput', false);
-    yBaselineLabel = arrayfun(@(x) floatToLabel(x, thresholdSel), yBaseline, 'UniformOutput', false);
-    [~, precLinear, ~] = fScore(yDevLabel, yPredLabel, 'doomed', 'successful');
-    [~, precBaseline, ~] = fScore(yDevLabel, yBaselineLabel, 'doomed', 'successful');
+    yPredLabel = arrayfun(@(x) floatToLabel(x, thresholdSel), yPred, ...
+        'UniformOutput', false);
+    yBaselineLabel = arrayfun(@(x) floatToLabel(x, thresholdSel), ...
+        yBaseline, 'UniformOutput', false);
+    [~, precLinear, ~] = fScore(yDevLabel, yPredLabel, 'doomed', ...
+        'successful');
+    [~, precBaseline, ~] = fScore(yDevLabel, yBaselineLabel, 'doomed', ...
+        'successful');
     threshold(i) = thresholdSel;
     precisionLinear(i) = precLinear;
     precisionBaseline(i) = precBaseline;
     thresholdSel = thresholdSel + thresholdStep;
 end
 disp(table(threshold, precisionLinear, precisionBaseline));
+%% failure analysis
+
+yDifference = abs(yDev - yPred);
+[yDifferenceSorted, sortIndex] = sort(yDifference, 'descend');
+
+numFramesToClip = 100;
+fprintf('First %d frames with largest difference:\n', numFramesToClip);
+
+clipDir = sprintf('%s\\clips', pwd);
+[status, msg, msgID] = mkdir(clipDir);
+
+for i = 1:numFramesToClip
+    
+    frameNumDev = sortIndex(i);
+    trackNum = matchingTrackNumsDev(frameNumDev);
+    frameTime = matchingFrameTimesDev(frameNumDev);
+    track = trackListDev{trackNum};
+
+    fprintf('clipFilename=%d.wav frameNumDev=%d time=%s trackNum=%d trackFilename=%s\n', ...
+        i, frameNumDev, char(frameTime), trackNum, track.filename);
+    
+    [audioData, sampleRate] = audioread(track.filename);
+    
+    clipSizeSeconds = 1;
+    timeStart = frameTime - seconds(clipSizeSeconds/2);
+    timeEnd = frameTime + seconds(clipSizeSeconds/2);
+    idxStart = round(seconds(timeStart) * sampleRate);
+    idxEnd = round(seconds(timeEnd) * sampleRate);
+    
+    newFilename = sprintf('%s\\%d.wav', clipDir, i);
+    clipData = audioData(idxStart:idxEnd);
+    
+    audiowrite(newFilename, clipData, sampleRate);
+
+end
+
 %%
 % mae = @(A, B) (mean(abs(A - B)));
 % fprintf('Regressor MAE = %f\n', mae(yDev, yPred));
