e341856c2fa5fe254a149399bcfafb30feb6b398
Jonathan Avila <jonathan.edav@gmail.com>
2021-04-20 10:23:26 -0600

    Add option to use time feature

--- src/getXYfromTrackList.m
+++ src/getXYfromTrackList.m
@@ -1,7 +1,12 @@
 function [X, y, frameTrackNums, frameTimes, frameUtterances] = ...
     getXYfromTrackList(trackList, featureSpec, useAllAnnotators)
 % GETXYFROMTRACKLIST Features are stored in X, labels are stored in y. 
-% See also GETXYFROMFILE.
+% See also GETXYFROMFILE. If addTimeFeature is true, the last column of X
+% is a time feature indicating the time of each frame relative its dialog.
+% It is a temporary solution while we decide whether to keep this feature.
+
+    % config
+    addTimeFeature = true;
 
     X = [];
     y = [];
@@ -21,11 +26,15 @@ function [X, y, frameTrackNums, frameTimes, frameUtterances] = ...
             getXYfromFile(track.filename, featureSpec, useAllAnnotators);
         toc;
         
+        if addTimeFeature
+            dialogX = [dialogX milliseconds(dialogFrameTimes)];
+        end
+        
         % TODO appending is slow
         X = [X; dialogX];
         y = [y; dialogY];
-        toAppend = ones([size(dialogX, 1) 1]) * trackNum;
-        frameTrackNums = [frameTrackNums; toAppend];
+        trackNumsToAppend = ones([size(dialogX, 1) 1]) * trackNum;
+        frameTrackNums = [frameTrackNums; trackNumsToAppend];
         frameTimes = [frameTimes; dialogFrameTimes];
         frameUtterances = [frameUtterances; dialogFrameUtterances];
     end
