77caf2b4477e745d852ff4128786480f4b83342d
Jonathan Avila <jonathan.edav@gmail.com>
2021-04-27 11:33:36 -0600

    Update predict on the compare set instead of the dev set

--- src/kNNframeLevel.m
+++ src/kNNframeLevel.m
@@ -5,10 +5,13 @@ prepareData;
 %% train
 model = fitcknn(Xtrain, yTrain);
 
-%% predict on the dev set
-yPred = predict(model, Xdev);
+%% predict on the compare (dev or test) set
+yPred = predict(model, Xcompare);
 %% fscore
-% TODO update the fscore function to handle integers
+
+% Output as of April 27, 2021
+% beta=0.25, fscore=0.281, precision=0.273, recall=0.524
+
 threshold = 0.5;
 yCompareLabel = arrayfun(@(x) floatToLabel(x, threshold), yCompare, ...
     'UniformOutput', false);
@@ -16,10 +19,18 @@ yPredLabel = arrayfun(@(x) floatToLabel(x, threshold), yPred, ...
     'UniformOutput', false);
 classPositive = 'doomed';
 classNegative = 'successful';
+
+beta = 0.25;
 [score, precision, recall] = fScore(yCompareLabel, yPredLabel, ...
-    classPositive, classNegative);
-fprintf('f1score=%.3f, precision=%.3f, recall=%.3f\n', score, precision, recall);
+    classPositive, classNegative, beta);
+fprintf('beta=%.2f, fscore=%.3f, precision=%.3f, recall=%.3f\n', beta, score, precision, recall);
+
 %% more stats
+
+% Output as of April 27, 2021
+% k-NN MAE = 0.480002
+% k-NN MSE = 0.480002
+
 mae = @(A, B) (mean(abs(A - B)));
 mse = @(A, B) (mean((A - B) .^ 2));
 fprintf('k-NN MAE = %f\n', mae(yPred, yCompare));
