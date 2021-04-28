99b916528842771e4304786834bbe42cee3f4f2b
Jonathan Avila <jonathan.edav@gmail.com>
2021-04-28 09:53:44 -0600

    Add allow more labels

--- src/labelToFloat.m
+++ src/labelToFloat.m
@@ -1,9 +1,11 @@
 function float = labelToFloat(label)
-    if strcmp(label, "n") || strcmp(label, "nn")
+    if strcmp(label, "n") || strcmp(label, "nn") || ...
+            strcmp(label, "successful")
         float = 0;
-    elseif strcmp(label, "d") || strcmp(label, "dd")
+    elseif strcmp(label, "d") || strcmp(label, "dd") || ...
+            strcmp(label, "doomed_1") || strcmp(label, "doomed_2")
         float = 1;
     else
-        error('unknown label encountered: "%s"\n', label);
-    end
+        error('labelToFloat: unknown label: "%s"\n', label);
+    end    
 end
