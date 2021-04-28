75056c515d184b104c3eef41d742780eb79fd9dd
Jonathan Avila <jonathan.edav@gmail.com>
2021-04-28 09:54:11 -0600

    Update move code to genHistogramForModel.m

--- src/generateHistogramsModels.m
+++ src/generateHistogramsModels.m
@@ -53,56 +53,9 @@ genHistogramForModel(predN, predD, 'logistic regression dev');
 kNNmodel = fitcknn(Xtrain, yTrain);
 predN = predict(kNNmodel, trainNeutral);
 predD = predict(kNNmodel, trainDissatisfied);
-%%
 genHistogramForModel(predN, predD, 'kNN train'); % TODO fix display bug
 
-%% predict on dev set
+% predict on dev set
 predN = predict(kNNmodel, devNeutral);
 predD = predict(kNNmodel, devDissatisfied);
-%%
 genHistogramForModel(predN, predD, 'kNN dev');
-
-function genHistogramForModel(predN, predD, modelName)
-
-    NBINS = 32;
-    BARCOLORN = '#1e88e5'; 
-    BARCOLORD = '#fb8c00';
-    IMAGEDIR = append(pwd, "\src\histograms-models\");
-    
-    mkdir(IMAGEDIR);
-
-    f = figure('Visible', 'off');
-
-    % histogram for predictions on neutral frames
-    hPredN = histogram(predN, NBINS);
-    hPredN.FaceColor = BARCOLORN;
-
-    hold on
-
-    % histogram for predictions on dissatisfied frames
-    hPredD = histogram(predD, NBINS);
-    hPredD.FaceColor = BARCOLORD;
-
-    % normalize the histograms so that all bar heights add to 1
-    hPredN.Normalization = 'probability';
-    hPredD.Normalization = 'probability';
-
-    % adjust bars so that both plots align
-    hPredN.BinWidth = hPredD.BinWidth;
-    hPredN.BinEdges = hPredD.BinEdges;
-
-    % add titles, axes labels, and legend
-    titleText = modelName;
-    subtitleText = sprintf('Predictions on dev set, nBins=%d', NBINS);
-    title(titleText, subtitleText);
-    ylabel('Number in bin');
-    xlabel('Bin');
-    lgd = legend('neutral','dissatisfied');
-    lgd.Location = 'best';
-    
-
-    % save image
-    imageFilepath = append(IMAGEDIR, titleText, ".png");
-    saveas(f, imageFilepath);
-    fprintf('Saved regressor output histogram to %s\n', imageFilepath);
-end
