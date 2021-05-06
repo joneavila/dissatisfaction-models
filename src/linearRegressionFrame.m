753a6bf053136c4d99ad63a32025e2b2f850111d
Jonathan Avila <jonathan.edav@gmail.com>
2021-05-05 18:05:05 -0600

    Refactor

--- src/linearRegressionFrame.m
+++ src/linearRegressionFrame.m
@@ -1,84 +1,58 @@
-% linearRegression.m Frame-level linear regression model
-
-%% prepare the data
-prepareData;
+% linearRegressionFrame.m Frame-level linear regression model
 
 %% train regressor
-model = fitlm(Xtrain, yTrain);
-save('linearRegressor.mat', 'model');
+prepareData;
+linearRegressor = fitlm(Xtrain, yTrain);
 
-%% save coefficient info to a text file
-outputFilename = append(pwd, '/src/coefficients-extended.txt');
-fileID = fopen(outputFilename, 'w');
-coefficients = model.Coefficients.Estimate;
+%% save the model to use in the dialog-level model later
+modelFilename = 'linearRegressor.mat';
+save(modelFilename, 'linearRegressor');
+fprintf('Saved trained linear regressor as %s\n', modelFilename);
+
+%% print out coefficient info
+coefficients = linearRegressor.Coefficients.Estimate;
 coefficients(1) = []; % discard the first coefficient (intercept)
 [coefficientSorted, coeffSortedIdx] = sort(coefficients, 'descend');
-fprintf(fileID, 'Coefficients in descending order with format:\n');
-fprintf(fileID, 'coefficient number, value, abbreviation\n');
+fprintf('Coefficients in descending order with format:\n');
+fprintf('coefficient number, value, abbreviation\n');
 for i = 1:length(coefficients)
     coeffNum = coeffSortedIdx(i);
     coeffValue = coefficientSorted(i);
-    try
-        coeffAbbrev = featureSpec(coeffNum).abbrev;
-    catch
-        % this warning is only shown when X contains the time feature
-        % it is a temporary solution
-        warning('Coefficient number not in featureSpec, out of bounds')
-        coeffAbbrev = 'out-of-bounds';
-    end
-    fprintf(fileID, '%2d | %f | %s\n', coeffNum, coeffValue, coeffAbbrev);
+    coeffAbbrev = featureSpec(coeffNum).abbrev;
+    fprintf('%2d | %f | %s\n', coeffNum, coeffValue, coeffAbbrev);
 end
-fclose(fileID);
-fprintf('Coefficients saved to %s\\%s\n', pwd, outputFilename);
 
 %%  predict on the compare set
-yPred = predict(model, Xcompare);
+yPred = predict(linearRegressor, Xcompare);
 
-% the baseline always predicts dissatisfied (positive class)
-yBaseline = ones([size(Xcompare, 1), 1]);
+% the baseline always predicts dissatisfied (1 for positive class)
+yBaseline = ones([length(yPred), 1]);
 %% print f1 score and more for different thresholds
 
-% Output as of April 27, 2021
-% beta=0.25, bestThreshold=0.555, bestLinearFscore=0.31, ...
-% baselineFscoreAtBestThreshold=0.27
+% Output as of May 5, 2021:
+%  beta=0.25, bestThreshold=0.555, bestLinearFscore=0.31, ...
+%  baselineFscoreAtBestThreshold=0.27
 
 thresholdMin = min(yPred);
 thresholdMax = max(yPred);
-thresholdNum = 500;
+thresholdNum = 500; % change this back to 500
 thresholdStep = (thresholdMax - thresholdMin) / thresholdNum;
 
 beta = 0.25;
-
-thresholdCompare = 0.5;
-yCompareLabel = arrayfun(@(x) floatToLabel(x, thresholdCompare), yCompare, ...
-    'UniformOutput', false);
 
 bestLinearFscore = 0;
 baselineFscoreAtBestThreshold = 0;
 bestThreshold = 0;
 
 for threshold = thresholdMin:thresholdStep:thresholdMax
-
-    yPredLabel = arrayfun(@(x) floatToLabel(x, threshold), yPred, ...
-        'UniformOutput', false);
-    [scoLinear, precLinear, recLinear] = fScore(yCompareLabel, ...
-        yPredLabel, 'doomed', 'successful', beta);
-
-    if scoLinear >= bestLinearFscore
-        bestLinearFscore = scoLinear;
+    yPredAfterThreshold = yPred >= threshold;
+    [scoreModel, ~, ~] = fScore(yCompare, yPredAfterThreshold, 1, 0, beta);
+    if scoreModel >= bestLinearFscore
+        bestLinearFscore = scoreModel;
         bestThreshold = threshold;
-        
-        yBaselineLabel = arrayfun(@(x) floatToLabel(x, threshold), ...
-        yBaseline, 'UniformOutput', false);
-        [scoBaseline, precBaseline, recBaseline] = fScore(yCompareLabel, ...
-        yBaselineLabel, 'doomed', 'successful', beta);
-        
-        baselineFscoreAtBestThreshold = scoBaseline;
+        [scoreBaseline, ~, ~] = fScore(yCompare, yBaseline, 1, 0, beta);
+        baselineFscoreAtBestThreshold = scoreBaseline;
     end
- 
-    % fprintf('scoLinear=%.2f scoBaseline=%.2f precLinear=%.2f precBaseline=%.2f recLinear=%2.f recBaseline=%.2f\n', ...
-    %     scoLinear, scoBaseline, precLinear, precBaseline, recLinear, recBaseline);
-    
 end
 
 fprintf('beta=%.2f, bestThreshold=%.3f, bestLinearFscore=%.2f, baselineFscoreAtBestThreshold=%.2f\n', ...
@@ -86,12 +60,12 @@ fprintf('beta=%.2f, bestThreshold=%.3f, bestLinearFscore=%.2f, baselineFscoreAtB
 
 %% print stats
 
-% Output as of April 27, 2021
-% Regressor MAE = 0.433054
-% Baseline MAE = 0.743079
-% Linear regressor MSE = 0.250489
-% Baseline MSE = 0.743079
-% Regressor R-squared = 0.347917
+% Output as of April 27, 2021:
+%  Regressor MAE = 0.433054
+%  Baseline MAE = 0.743079
+%  Linear regressor MSE = 0.250489
+%  Baseline MSE = 0.743079
+%  Regressor R-squared = 0.347917
 
 mae = @(A, B) (mean(abs(A - B)));
 fprintf('Regressor MAE = %f\n', mae(yCompare, yPred));
@@ -101,13 +75,15 @@ mse = @(actual, pred) (mean((actual - pred) .^ 2));
 fprintf('Linear regressor MSE = %f\n', mse(yCompare, yPred));
 fprintf('Baseline MSE = %f\n\n', mse(yCompare, yBaseline));
 
-fprintf('Regressor R-squared = %f\n', model.Rsquared.adjusted);
+fprintf('Regressor R-squared = %f\n', linearRegressor.Rsquared.adjusted);
+
 %% failure analysis
 
 % config
-clipSizesSeconds = [1 2];
+clipSizeSeconds = 6;
 numClipsToCreate = 20;
 ignoreSizeSeconds = 2;
+
 sortDirections = ["descend" "ascend"];
 
 yDifference = abs(yCompare - yPred);
@@ -149,16 +125,14 @@ for sortDirNum = 1:size(sortDirections, 2)
         track = trackListCompare{trackNum};
         [audioData, sampleRate] = audioread(track.filename);
 
-        for clipSizeIdx = 1:length(clipSizesSeconds)
-            clipSizeSeconds = clipSizesSeconds(clipSizeIdx);
-            timeStart = frameTime - seconds(clipSizeSeconds/2);
-            timeEnd = frameTime + seconds(clipSizeSeconds/2);
-            idxStart = round(seconds(timeStart) * sampleRate);
-            idxEnd = round(seconds(timeEnd) * sampleRate);
-            newFilename = sprintf('%s\\clip%d-%dseconds.wav', clipDir, frameNumCompare, clipSizeSeconds);
-            clipData = audioData(idxStart:idxEnd);
-            audiowrite(newFilename, clipData, sampleRate);
-        end
+        timeStart = frameTime - seconds(clipSizeSeconds/2);
+        timeEnd = frameTime + seconds(clipSizeSeconds/2);
+        idxStart = round(seconds(timeStart) * sampleRate);
+        idxEnd = round(seconds(timeEnd) * sampleRate);
+        newFilename = sprintf('%s\\clip%d-%dseconds.wav', clipDir, frameNumCompare, clipSizeSeconds);
+        clipData = audioData(idxStart:idxEnd);
+        audiowrite(newFilename, clipData, sampleRate);
+
 
         fprintf(fileID, 'clip%d  timeSeconds=%.2f  filename=%s\n', ...
             frameNumCompare, seconds(frameTime), track.filename);
@@ -181,18 +155,14 @@ for sortDirNum = 1:size(sortDirections, 2)
         end
 
         for frameNumProbe = frameNumCompareStart:frameNumCompareEnd
-            
-            % check if this frame is in the same track as the original
+            % if this frame is in the same track and utterance as the 
+            % original, mark the frame to ignore it
             if frameTrackNumsCompare(frameNumProbe) ~= frameTrackNumsCompare(frameNumCompare)
                 continue;
             end
-
-            % check if this frame is in the same utterance as the original
             if frameUtterancesCompare(frameNumProbe) ~= frameUtterancesCompare(frameNumCompare)
                 continue;
             end
-
-            % if both are true, mark the frame to zero it out
             framesToIgnore(frameNumProbe) = 1;
         end
     end
