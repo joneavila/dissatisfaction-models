55e3c313d56ef3f311675168e2f68d9bb9b924ea
Jonathan Avila <jonathan.edav@gmail.com>
2021-05-20 18:37:18 -0600

    Remove useAllAnnotators argument and option to use time feature

--- source/getXYfromTrackList.m
+++ source/getXYfromTrackList.m
@@ -1,12 +1,9 @@
 function [X, y, frameTrackNums, frameTimes, frameUtterances] = ...
-    getXYfromTrackList(trackList, featureSpec, useAllAnnotators)
+    getXYfromTrackList(trackList, featureSpec)
 % GETXYFROMTRACKLIST Features are stored in X, labels are stored in y. 
 % See also GETXYFROMFILE. If addTimeFeature is true, the last column of X
 % is a time feature indicating the time of each frame relative its dialog.
 % It is a temporary solution while we decide whether to keep this feature.
-
-    % config
-    addTimeFeature = false;
 
     X = [];
     y = [];
@@ -21,14 +18,18 @@ function [X, y, frameTrackNums, frameTimes, frameUtterances] = ...
         
         fprintf('[%d/%d] Getting X and y for %s\n', trackNum, nTracks, ...
             track.filename);
-        tic;
-        [dialogX, dialogY, dialogFrameTimes, dialogFrameUtterances] = ...
-            getXYfromFile(track.filename, featureSpec, useAllAnnotators);
-        toc;
         
-        if addTimeFeature
-            dialogX = [dialogX milliseconds(dialogFrameTimes)];
+        % ignore if the annotation file does not exist
+        [~, name, ~] = fileparts(track.filename);
+        annFilename = append(name, ".txt");
+        annotationFilename = append(pwd, '\annotations\', annFilename);
+        if ~isfile(annotationFilename)
+            fprintf('\tannotation file not found, skip\n');
+            continue
         end
+    
+        [dialogX, dialogY, dialogFrameTimes, dialogFrameUtterances] = ...
+            getXYfromFile(track.filename, featureSpec);
         
         % TODO appending is slow
         X = [X; dialogX];
