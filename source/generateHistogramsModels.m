87c13db4b49e25709a6fc93fc07be1437720b9c6
Jonathan Avila <jonathan.edav@gmail.com>
2021-06-02 11:37:45 -0600

    Update generate histogram for frame-level model on all data sets

--- source/generateHistogramsModels.m
+++ source/generateHistogramsModels.m
@@ -1,61 +1,82 @@
 % generateHistogramsModels.m
-% save a histogram for the various models' predictions on neutral frames
-% versus dissatisfied frames
+% save a histogram for the frame-level model's predictions on neutral 
+% frames versus dissatisfied frames
 
-%% prepare data
-prepareData;
+% prepareData;
 
-% 'n' and 'nn' are the negative class (0), 'd' and 'dd' are the positive
-% class (1)
-trainNeutral = Xtrain(yTrain==0, :);
-trainDissatisfied = Xtrain(yTrain==1, :);
+classNeutral = 0;
+classDiss = 1;
 
-devNeutral = Xdev(yDev==0, :);
-devDissatisfied = Xdev(yDev==1, :);
+XtrainNeutral = XtrainFrame(yTrainFrame==classNeutral, :);
+XtrainDiss = XtrainFrame(yTrainFrame==classDiss, :);
 
-% t-test will compare neutral and dissatisfied
-N = [trainNeutral; devNeutral];
-D = [trainDissatisfied; devDissatisfied];
+XdevNeutral = XdevFrame(yDevFrame==classNeutral, :);
+XdevDiss = XdevFrame(yDevFrame==classDiss, :);
 
-%% linear regression
+XtestNeutral = XtestFrame(yTestFrame==classNeutral, :);
+XtestDiss = XtestFrame(yTestFrame==classDiss, :);
 
 % predict on train set
-linearRegressor = fitlm(Xtrain, yTrain);
-predN = predict(linearRegressor, trainNeutral);
-predD = predict(linearRegressor, trainDissatisfied);
-genHistogramForModel(predN, predD, 'linear regression train');
+linearRegressor = fitlm(XtrainFrame, yTrainFrame);
+predsNeutral = predict(linearRegressor, XtrainNeutral);
+predsDiss = predict(linearRegressor, XtrainDiss);
+genHistogramForModel(predsNeutral, predsDiss, ...
+    'Linear regressor output (train set)');
 
 % predict on dev set
-predN = predict(linearRegressor, devNeutral);
-predD = predict(linearRegressor, devDissatisfied);
-genHistogramForModel(predN, predD, 'linear regression dev');
+predsNeutral = predict(linearRegressor, XdevNeutral);
+predsDiss = predict(linearRegressor, XdevDiss);
+genHistogramForModel(predsNeutral, predsDiss, ...
+    'Linear regressor output (dev set)');
 
-%% logistic regression
+% predict on test set
+predsNeutral = predict(linearRegressor, XtestNeutral);
+predsDiss = predict(linearRegressor, XtestDiss);
+genHistogramForModel(predsNeutral, predsDiss, ...
+    'Linear regressor output (test set)');
 
-% predict on train set
-coeffEstimates = mnrfit(Xtrain, yTrain+1);
-pihat = mnrval(coeffEstimates, trainNeutral);
-predN = pihat(:, 2);
-pihat = mnrval(coeffEstimates, trainDissatisfied);
-predD = pihat(:, 2);
-genHistogramForModel(predN, predD, 'logistic regression train');
+function genHistogramForModel(predN, predD, modelName)
 
-% predict on dev set
-pihat = mnrval(coeffEstimates, devNeutral);
-predN = pihat(:, 2);
-pihat = mnrval(coeffEstimates, devDissatisfied);
-predD = pihat(:, 2);
-genHistogramForModel(predN, predD, 'logistic regression dev');
+    % config
+    NBINS = 32;
+    BARCOLORN = '#1e88e5'; 
+    BARCOLORD = '#fb8c00';
+    IMAGEDIR = append(pwd, "\source\histograms-models\");
+    
+    if ~exist(IMAGEDIR, 'dir')
+        mkdir(IMAGEDIR)
+    end
 
-%% k-NN
+    f = figure('Visible', 'off');
 
-% predict on train set
-kNNmodel = fitcknn(Xtrain, yTrain);
-predN = predict(kNNmodel, trainNeutral);
-predD = predict(kNNmodel, trainDissatisfied);
-genHistogramForModel(predN, predD, 'kNN train'); % TODO fix display bug
+    % histogram for predictions on neutral frames
+    hPredN = histogram(predN, NBINS);
+    hPredN.FaceColor = BARCOLORN;
 
-% predict on dev set
-predN = predict(kNNmodel, devNeutral);
-predD = predict(kNNmodel, devDissatisfied);
-genHistogramForModel(predN, predD, 'kNN dev');
+    hold on
+
+    % histogram for predictions on dissatisfied frames
+    hPredD = histogram(predD, NBINS);
+    hPredD.FaceColor = BARCOLORD;
+
+    % normalize the histograms so that all bar heights add to 1
+    hPredN.Normalization = 'probability';
+    hPredD.Normalization = 'probability';
+
+    % adjust bars so that both plots align
+    hPredN.BinWidth = hPredD.BinWidth;
+    hPredN.BinEdges = hPredD.BinEdges;
+
+    % add titles, axes labels, and legend
+    titleText = modelName;
+    title(titleText);
+    ylabel('Probability');
+    xlabel('Dissatisfaction bin');
+    lgd = legend('neutral','dissatisfied');
+    lgd.Location = 'best';
+    
+    % save image
+    imageFilepath = append(IMAGEDIR, titleText, ".png");
+    saveas(f, imageFilepath);
+    fprintf('Saved histogram to %s\n', imageFilepath);
+end
