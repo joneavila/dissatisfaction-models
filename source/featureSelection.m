cc9f644da30d8dc63c3f7e3f350f73c5a5d5463d
Jonathan E Avila <jonathan.edav@gmail.com>
2021-08-11 20:31:37 -0600

    Update featureSelection.m

--- source/featureSelection.m
+++ source/featureSelection.m
@@ -1,63 +1,93 @@
 % featureSelection.m
+% Get results for dialog-level model using all combination of summary
+% features listed below as featureNames.
 
 prepareData;
 
-% train first regressor
+% train first regressor, used to predict dissatisfaction from prosody
+% features
 firstRegressor = fitlm(XtrainDialog, yTrainDialog);
 
-% get summary features for the second regressor
-[XsummaryTrain, yTrain] = getSummaryXYfromTracklist(tracklistTrainDialog, ...
+% get summary features for the second regressor, used to predict
+% dissatisfaction from summary features
+tracklistCombined = [tracklistTrainDialog tracklistDevDialog];
+[Xcombined, yCombined] = getSummaryXYfromTracklist(tracklistCombined, ...
     centeringValuesDialog, scalingValuesDialog, firstRegressor, useTimeFeature);
-[XsummaryDev, yDev] = getSummaryXYfromTracklist(tracklistDevDialog, ...
-centeringValuesDialog, scalingValuesDialog, firstRegressor, useTimeFeature);
 
 % features in same order as getSummaryXYfromTracklist.m
 featureNames = ["ratio", "min", "max", "average", "range", "std"];
 
-varNames = ["comboString", "F-score", "precision", "recall"];
+varNames = ["comboString", "F-score (average)"];
 sz = [0 length(varNames)];
-varTypes = ["string", "double", "double", "double"];
-resultTable = table('Size',sz,'VariableTypes',varTypes,'VariableNames',varNames);
+varTypes = ["string", "double"];
+resultsTable = table('Size',sz,'VariableTypes',varTypes,'VariableNames',varNames);
 
-numFeatures = size(XsummaryTrain, 2);
+numTracks = size(Xcombined, 1);
+numFeatures = size(Xcombined, 2);
+
+% create a list (cell array) of feature index combinations
+combinationsList = {};
 featureSet = 1:numFeatures;
-
 for featureSubsetSize = 1:numFeatures
     C = nchoosek(featureSet, featureSubsetSize);
-    
     for i = 1:size(C, 1)
         combination = C(i,:);
+        combinationsList(end+1) = {combination};
+    end
+end
+
+% for each combination of summary features
+%%
+numCombinations = length(combinationsList);
+for combinationNum = 1:numCombinations
+    
+    combination = combinationsList(combinationNum);
+    combination = cell2mat(combination);
+    
+    % create a string to refer to the combination in result table
+    featureNamesInCombo = featureNames(combination);
+    comboString = strjoin(featureNamesInCombo, '+');
+    
+    fprintf('[%d/%d] %s\n', combinationNum, numCombinations, comboString);
+    
+    % for each dialog
+    scores = zeros([numTracks 1]);
+    for trackNum = 1:numTracks
+    
+        % select this dialog to predict on and
+        % use the rest for training
+        Xtrain = Xcombined;
+        Xtrain(trackNum,:) = [];
+        yTrain = yCombined;
+        yTrain(trackNum) = [];
+        Xdev = Xcombined(trackNum,:);
+        yDev = yCombined(trackNum);
         
-        % create combination string to refer to in result table
-        featureNamesInCombo = featureNames(combination);
-        comboString = strjoin(featureNamesInCombo, '+');
+        % keep only the features in the combination
+        Xtrain = Xtrain(:,combination);
+        Xdev = Xdev(:,combination);
         
-        % get subset of the data
-        Xtrain = XsummaryTrain(:,combination);
-        Xdev = XsummaryDev(:,combination);
-        
-        % train
+        % train the second regressor
         secondRegressor = fitlm(Xtrain, yTrain);
         
-        % predict
+        % predict on the single dialog
         yPred = predict(secondRegressor, Xdev);
         
-        % get result
-        threshold = 0.5;
-        beta = 0.25;
+        threshold = 0.65;
         yPredAfterThreshold = yPred >= threshold;
-        [score, precision, recall] = fScore(yDev, yPredAfterThreshold, ...
-            1, 0, beta);
+        beta = 0.25;
+        [score, precision, recall] = fScore(yDev, yPredAfterThreshold, 1, 0, beta);
         
-        % add result to table
-        rowToAdd = {comboString, score, precision, recall};
-        resultTable = [resultTable; rowToAdd];
-         
+        scores(trackNum) = score;
+
     end
     
-    % sort table
-    sortColumn = 2;
-    direction = 'descend';
-    resultTableSorted = sortrows(resultTable, sortColumn, direction);
-  
+    % add the mean F-score to the results table
+    scoreAverage = mean(scores);
+    rowToAdd = {comboString, scoreAverage};
+    resultsTable = [resultsTable; rowToAdd];
+    
 end
+
+% sort the table by F-score
+resultsTable = sortrows(resultsTable, 2, 'descend');
