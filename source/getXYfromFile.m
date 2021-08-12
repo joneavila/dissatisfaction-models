2025f0dc45720aea6bd7e2312efdb3a4e6cf9c35
Jonathan E Avila <jonathan.edav@gmail.com>
2021-08-11 20:36:28 -0600

    Refactor: no need for functions and nested for loops

--- source/getXYfromFile.m
+++ source/getXYfromFile.m
@@ -1,89 +1,48 @@
-function [X, y, frameTimes, frameUtterances] = getXYfromFile(filename, ...
-    featureSpec)
+function [X, y, frameUtterances] = getXYfromFile(filename, featureSpec)
 % GETXYFROMFILE Features are stored in X and labels are stored in y. 
 % For frame i, frameTimes(i) is the time in milliseconds and
-% frameUtterances(i) is the frame's utterance number (if the utterances in 
-% X were labeled 1..n)
+% frameUtterances(i) is the frame's utterance number (utterances in 
+% X are labeled 1..n)
     
-    % get the annotation filename from the dialog filename, assuming
-    % they have the same name
+    % get the annotation filename from the dialog filename, assumin they 
+    % have the same name, then get the annotation table
     [~, name, ~] = fileparts(filename);
     annFilename = append(name, ".txt");
-    
-    % get the monster
-    customerSide = 'l';
-    trackSpec = makeTrackspec(customerSide, filename, '.\calls\');
-    [~, monster] = makeTrackMonster(trackSpec, featureSpec);
-
-    % get the annotation table
-    annotationPath = append('annotations\', annFilename);
+    annotationPath = append('annotations/', annFilename);
     useFilter = true;
     annotationTable = readElanAnnotation(annotationPath, useFilter);
     
-    % TODO replace with arrayfun
-    % mark all frames that are annotated
+    % get the monster
+    customerSide = 'l';
+    trackSpec = makeTrackspec(customerSide, filename, './calls/');
+    [~, monster] = makeTrackMonster(trackSpec, featureSpec);
+    
     nFrames = size(monster, 1);
-    isFrameAnnotated = false([nFrames 1]);
-    for frameNum = 1:size(monster, 1)
-        if isFrameInTable(frameNum, annotationTable)
-            isFrameAnnotated(frameNum) = true;
-        end
+    
+    isFrameAnnotated = false([nFrames 1]); % assume frame is not annotated (false)
+    y = ones([nFrames 1]) * -1; % assume frame label does not exist (-1)
+    frameUtterances = ones([nFrames 1]) * -1; % assume frame does not belong to labeled utterance (-1)
+        
+    nRows = size(annotationTable, 1);
+    for rowNum = 1:nRows
+        row = annotationTable(rowNum, :);
+        frameStart = round(milliseconds(row.startTime) / 10);
+        frameEnd = round(milliseconds(row.endTime) / 10);
+        isFrameAnnotated(frameStart:frameEnd) = true;
+        y(frameStart:frameEnd) = labelToFloat(row.label);
+        frameUtterances(frameStart:frameEnd) = rowNum;
     end
 
-    y = getYfromTable(annotationTable, monster, isFrameAnnotated);
-
+    % TODO remove isFrameAnnotated and use y directly
     matchingFrameNums = find(isFrameAnnotated);
-    
+
+    X = monster(isFrameAnnotated, :);
+    y = y(isFrameAnnotated);
     frameTimes = arrayfun(@(frameNum) frameNumToTime(frameNum), ...
         matchingFrameNums);
+    frameUtterances = frameUtterances(isFrameAnnotated);
     
-    frameUtterances = arrayfun(@(frameNum) ...
-        frameToUtterance(frameNum, annotationTable), matchingFrameNums);
-    
-    X = monster(isFrameAnnotated, :);
+    % NEW - frameTimes is now a feature included in X
+    X = [X seconds(frameTimes)];
 
-end
-
-function utteranceNum = frameToUtterance(frameNum, annTable)
-    numRows = size(annTable, 1);
-    utteranceNum = -1;
-    for rowNum = 1:numRows
-        row = annTable(rowNum, :);
-        frameStart = round(milliseconds(row.startTime) / 10);
-        frameEnd = round(milliseconds(row.endTime) / 10);
-        if frameNum >= frameStart && frameNum <= frameEnd
-            utteranceNum = rowNum;
-            return;
-        end
-    end
-end
-
-function [inTable, annotation] = isFrameInTable(frameNum, annTable)
-% ISFRAMEINTABLE inTable is true if the frame is in the annotation, i.e.
-% the frame belongs to a labeled utterance. If inTable is true, annotation
-% is the the assigned label, else annotation is -1.
-
-    inTable = false;
-    annotation = -1;
-    for rowNum = 1:size(annTable, 1)
-        row = annTable(rowNum, :);
-        frameStart = round(milliseconds(row.startTime) / 10);
-        frameEnd = round(milliseconds(row.endTime) / 10);
-        if frameNum >= frameStart && frameNum <= frameEnd
-            inTable = true;
-            annotation = labelToFloat(row.label);
-            return;
-        end
-    end
-end
-
-function y = getYfromTable(annTable, monster, isFrameAnnotated)
-    y = ones([size(monster, 1) 1]) * -1;
-    for annotationNum = 1:height(annTable)
-        row = annTable(annotationNum, :);
-        frameStart = round(milliseconds(row.startTime) / 10);
-        frameEnd = round(milliseconds(row.endTime) / 10);
-        y(frameStart:frameEnd, :) = labelToFloat(row.label);
-    end
-    y = y(isFrameAnnotated);
 end
