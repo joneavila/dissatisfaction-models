048a54392be74a414753410fb49edbdf024037c1
Jonathan E Avila <jonathan.edav@gmail.com>
2021-08-12 18:30:19 -0600

    Remove unnecessary normalization, temporary bug fix, and other outdated code

--- source/getSummaryXYfromTracklist.m
+++ source/getSummaryXYfromTracklist.m
@@ -1,92 +1,62 @@
 function [Xsummary, yActual] = getSummaryXYfromTracklist(tracklist, ...
-    normalizeCenteringValues, normalizeScalingValues, firstRegressor, ...
-    useTimeFeature)
+    firstRegressor)
 
-    featureSpec = getfeaturespec('.\source\mono.fss');
     nTracks = size(tracklist, 2);
-
-    numSummaryFeatures = 3;
-    Xsummary = zeros([nTracks numSummaryFeatures]);
+    nSummaryFeatures = 3;
+    Xsummary = zeros([nTracks nSummaryFeatures]);
     yActual = zeros(size(tracklist));
     
-    if useTimeFeature
-        dataDir = append(pwd, '\data\monsters-with-time\');
-    else
-        dataDir = append(pwd, '\data\monsters-without-time\');
-    end
-    if ~exist(dataDir, 'dir')
-        mkdir(dataDir)
-    end
-
+    % TODO check if the directory exists
+    dataDir = append(pwd, '/data/dialog-level');
+    
     for trackNum = 1:nTracks
         
         track = tracklist{trackNum};
-        filename = track.filename;
-        fprintf('\t[%d/%d] %s... ', trackNum, nTracks, track.filename);
+        trackFilename = track.filename;
         
-        % get the annotation path, assuming they share the same name
-        [~, name, ~] = fileparts(filename);
-        annFilename = append(name, ".txt");
-        annotationPath = append('annotations\', annFilename);
+        annotationTable = readElanAnnotation(trackFilename, true);
         
-        % skip this dialog if the annotation file does not exist
-        if ~exist(annotationPath, 'file')
-            fprintf('annotation file not found\n');
-            continue
-        end
-
         % get the X for that dialog
-        % try to load the precomputed data, else compute it and save it for 
-        % future runs
-        customerSide = 'l';
-        
-        trackSpec = makeTrackspec(customerSide, filename, '.\calls\');
-        [~, name, ~] = fileparts(filename);
-        saveFilename = append(dataDir, name, '.mat');
+        % load precomputed data or throw error
+        [~, name, ~] = fileparts(trackFilename);
+        saveFilename = append(dataDir, '/', name, '.mat');
         try
             monster = load(saveFilename);
             monster = monster.monster;
         catch 
-            % [~, monster] = makeTrackMonster(trackSpec, featureSpec);
-            % save(saveFilename, 'monster');
-            error('monster not found: %s\n', filename);
+            error('monster not found: %s\n', trackFilename);
         end
         
-        % temporary bug fix
-        % rerun prepareData script to rebuild data/monsters-with-time
-        if size(monster,2) == 120
-            monster = monster(:,1:119);
-        elseif size(monster,2) == 118
-            monster(:,119) = monster(:,118);
-        end
-        
-
-        % normalize X (monster) using the same centering values and scaling 
-        % values used to normalize the data used for training
-        monster = normalize(monster, 'center', ...
-            normalizeCenteringValues, 'scale', normalizeScalingValues);
-        
-        % trim out-of-character frames from start and end of dialog
-        useFilter = false;
-        annotationTable = readElanAnnotation(annotationPath, useFilter);
+        %% trim out-of-character frames from start and end of dialog
         numRows = size(annotationTable, 1);
-        for rowNum = 1:numRows % find the first non-out-of-character
+        
+        % skip this track if there are no useable annotations for it
+        if ~numRows
+            continue;
+        end
+        
+        % find the first non-out-of-character
+        for rowNum = 1:numRows
             row = annotationTable(rowNum, :);
             if row.label ~= "o"
                 frameNumStart = round(milliseconds(row.startTime) / 10);
                 break
             end
         end
-        for rowNum = numRows:-1:1 % find the last non-out-of-character
+        
+        % find the last non-out-of-character
+        for rowNum = numRows:-1:1 
             row = annotationTable(rowNum, :);
             if row.label ~= "o"
                 frameNumEnd = round(milliseconds(row.startTime) / 10);
                 break
             end
         end
+        
+        % clear out-of-character frames
         monster = monster(frameNumStart:frameNumEnd, :);
 
-        % get the known Y for that dialog
+        %% get the known Y for that dialog
         % from call-log.xlsx, load the 'filename' and 'label' columns
         opts = spreadsheetImportOptions('NumVariables', 2, 'DataRange', ...
             'H2:I203', 'VariableNamesRange', 'H1:I1');
@@ -125,28 +95,27 @@ function [Xsummary, yActual] = getSummaryXYfromTracklist(tracklist, ...
         % feature 3 - standard deviation of dialogPred
         Xsummary(trackNum, 3) = std(dialogPred);
         
-        plotDirectory = append(pwd, "\time-pred-plots\");
-        if ~exist(plotDirectory, 'dir')
-            mkdir(plotDirectory)
-        end
-        
-        figWidth = 1920;
-        figHeight = 1080;
-        fig = figure('visible', 'off', 'position', ...
-            [0, 0, figWidth, figHeight]);
-        x = (1:length(dialogPred)) * milliseconds(10);
-        y = dialogPred;
-        plot(x, y);
-        % hold on
-        % plot(x, dialogActual);
-        % legend('dialogPred','dialogActual')
-        title(sprintf('%s\n', filename));
-        xlabel('time (seconds)');
-        ylabel('dissatisfaction');
-        ylim([-0.25 1.25]) % fix the y-axis range
-        exportgraphics(gca, sprintf('%s/%s.jpg', plotDirectory, name));
-        
-        fprintf('done\n');
+        %% plot predictions
+%         plotDirectory = append(pwd, "\time-pred-plots\");
+%         if ~exist(plotDirectory, 'dir')
+%             mkdir(plotDirectory)
+%         end
+%         
+%         figWidth = 1920;
+%         figHeight = 1080;
+%         fig = figure('visible', 'off', 'position', ...
+%             [0, 0, figWidth, figHeight]);
+%         x = (1:length(dialogPred)) * milliseconds(10);
+%         y = dialogPred;
+%         plot(x, y);
+%         % hold on
+%         % plot(x, dialogActual);
+%         % legend('dialogPred','dialogActual')
+%         title(sprintf('%s\n', trackFilename));
+%         xlabel('time (seconds)');
+%         ylabel('dissatisfaction');
+%         ylim([-0.25 1.25]) % fix the y-axis range
+%         exportgraphics(gca, sprintf('%s/%s.jpg', plotDirectory, name));
         
     end
 
