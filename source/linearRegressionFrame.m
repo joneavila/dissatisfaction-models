58f9eed787251a0e86a633870a5277671c12bd43
Jonathan E Avila <jonathan.edav@gmail.com>
2021-08-12 18:33:43 -0600

    Refactor failure analysis code

--- source/linearRegressionFrame.m
+++ source/linearRegressionFrame.m
@@ -14,9 +14,17 @@ regressor = fitlm(XtrainFrame, yTrainFrame);
 if useTestSet
     XcompareFrame = XtestFrame; %#ok<*UNRCH>
     yCompareFrame = yTestFrame;
+    trackNumsCompareFrame = trackNumsTestFrame;
+    tracklistCompareFrame = tracklistTestFrame;
+    frameTimesCompareFrame = frameTimesTestFrame;
+    utterNumsCompareFrame = utterNumsTestFrame;
 else
     XcompareFrame = XdevFrame;
     yCompareFrame = yDevFrame;
+    trackNumsCompareFrame = trackNumsDevFrame;
+    tracklistCompareFrame = tracklistDevFrame;
+    frameTimesCompareFrame = frameTimesDevFrame;
+    utterNumsCompareFrame = utterNumsDevFrame;
 end
 
 %% print coefficient info
@@ -47,7 +55,7 @@ yPred = predict(regressor, XcompareFrame);
 % the baseline always predicts dissatisfied (assume 1 for dissatisfied)
 yBaseline = ones(size(yPred));
 
-%% try different thresholds to find the best F-score
+%% try different dissatisfaction thresholds to find the best F-score
 mse = @(actual, pred) (mean((actual - pred) .^ 2));
 
 % try thresholdNum thresholds between thresholdMin and thresholdMax
@@ -62,8 +70,6 @@ varNames = {'threshold', 'mse', 'fscore', 'precision', 'recall'};
 sz = [thresholdNum, length(varNames)];
 resultTable = table('Size', sz, 'VariableTypes', varTypes, ...
     'VariableNames', varNames);
-
-
 
 % populate results table
 for thresholdNum = 1:length(thresholds)
@@ -103,96 +109,106 @@ baselineMSE = mse(yBaselineAfterThreshold, yCompareFrame);
 fprintf('baselineFscore=%.2f, baselinePrecision=%.2f, baselineRecall=%.2f, baselineMSE=%.2f\n', ...
     baselineFscore, baselinePrecision, baselineRecall, baselineMSE);
 
-% %% failure analysis
-% 
-% % config
-% clipSizeSeconds = 6;
-% numClipsToCreate = 20;
-% ignoreSizeSeconds = 2;
-% 
-% sortDirections = ["descend" "ascend"];
-% 
-% yDifference = abs(yCompare - yPred);
-% 
-% for sortDirNum = 1:size(sortDirections, 2)
-%     
-%     % sort yDifference following sort direction
-%     sortDirection = sortDirections(sortDirNum);    
-%     [~, sortIndex] = sort(yDifference, sortDirection);
-%     
-%     clipDir = sprintf('%s\\clips-extended-time-%s', pwd, sortDirection);
-%     [status, msg, msgID] = mkdir(clipDir);
-%     
-%     outputFilename = append(clipDir, '\output.txt');  % need to add rest of path
-%     fileID = fopen(outputFilename, 'w');
-%     
-%     fprintf(fileID, 'sortDirection=%s\n\n', sortDirection);
-% 
-%     framesToIgnore = zeros(size(yDifference));
-%     
-%     % create clips until numClipsCreated is reached or all frames have been
-%     % probed
-%     numClipsCreated = 0;
-%     for frameProbingNum = 1:length(sortIndex)
-%         
-%         if numClipsCreated >= numClipsToCreate
-%             break;
-%         end
-% 
-%         frameNumCompare = sortIndex(frameProbingNum);
-% 
-%         % ignore this frame if it has already been included in a clip
-%         if framesToIgnore(frameNumCompare)
-%             continue;
-%         end
-% 
-%         frameTime = frameTimesCompare(frameNumCompare);
-%         trackNum = frameTrackNumsCompare(frameNumCompare);
-%         track = trackListCompare{trackNum};
-%         [audioData, sampleRate] = audioread(track.filename);
-% 
-%         timeStart = frameTime - seconds(clipSizeSeconds/2);
-%         timeEnd = frameTime + seconds(clipSizeSeconds/2);
-%         idxStart = round(seconds(timeStart) * sampleRate);
-%         idxEnd = round(seconds(timeEnd) * sampleRate);
-%         newFilename = sprintf('%s\\clip%d-%dseconds.wav', clipDir, frameNumCompare, clipSizeSeconds);
-%         clipData = audioData(idxStart:idxEnd);
-%         audiowrite(newFilename, clipData, sampleRate);
-% 
-% 
-%         fprintf(fileID, 'clip%d  timeSeconds=%.2f  filename=%s\n', ...
-%             frameNumCompare, seconds(frameTime), track.filename);
-%         fprintf(fileID, '\tpredicted=%.2f  actual=%.2f\n', yPred(frameNumCompare), yCompare(frameNumCompare));
-%         
-%         numClipsCreated = numClipsCreated + 1;
-% 
-%         % zero out the 
-%         % check if any other frame number is within this frame's utterance
-%         clipSizeFrames = seconds(ignoreSizeSeconds) / milliseconds(10); % monster frames are 10ms
-%         frameNumCompareStart = frameNumCompare - clipSizeFrames / 2;
-%         frameNumCompareEnd = frameNumCompare + clipSizeFrames / 2;
-%         
-%         % adjust compare start and compare end if out of bounds
-%         if frameNumCompareStart < 1
-%             frameNumCompareStart = 1;
-%         end
-%         if frameNumCompareEnd > length(yDifference)
-%             frameNumCompareEnd = length(yDifference);
-%         end
-% 
-%         for frameNumProbe = frameNumCompareStart:frameNumCompareEnd
-%             % if this frame is in the same track and utterance as the 
-%             % original, mark the frame to ignore it
-%             if frameTrackNumsCompare(frameNumProbe) ~= frameTrackNumsCompare(frameNumCompare)
-%                 continue;
-%             end
-%             if frameUtterancesCompare(frameNumProbe) ~= frameUtterancesCompare(frameNumCompare)
-%                 continue;
-%             end
-%             framesToIgnore(frameNumProbe) = 1;
-%         end
-%     end
-%     
-%     fclose(fileID);
-%     fprintf('Output written to %s\n', outputFilename);
-% end
+%% failure analysis
+
+% configuration
+clipSizeSeconds = 6;
+nClipsToCreatePerDirection = 30;
+ignoreSizeSeconds = 2;
+
+yDifference = abs(yCompareFrame - yPred);
+
+sortDirections = ["descend" "ascend"];
+for sortDirNum = 1:size(sortDirections, 2)
+    
+    % sort yDifference in sortDirection
+    sortDirection = sortDirections(sortDirNum);    
+    [~, sortIndex] = sort(yDifference, sortDirection);
+    
+    % create the directory for this direction's clips
+    clipDir = sprintf('%s/failure-analysis/clips-%s', pwd, sortDirection);
+    [status, msg, msgID] = mkdir(clipDir);
+    
+    % create an output file to write clip details to
+    outputFilename = append(clipDir, '/clip-details.txt');
+    fileID = fopen(outputFilename, 'w');
+    
+    % write sort direction to file
+    fprintf(fileID, 'sortDirection=%s\n\n', sortDirection);
+
+    framesToIgnore = zeros(size(yDifference));
+    
+    % create clips until numClipsCreated is reached or 
+    % all frames have been probed
+    numClipsCreated = 0;
+    for i = 1:length(sortIndex)
+        
+        if numClipsCreated >= nClipsToCreatePerDirection
+            break;
+        end
+
+        frameNumToProbe = sortIndex(i);
+
+        % ignore this frame if it has already been included in a clip
+        % (within ignoreSizeSeconds of an existing clip)
+        if framesToIgnore(frameNumToProbe)
+            continue;
+        end
+        
+        frameTime = frameTimesCompareFrame(frameNumToProbe);
+        
+        trackNum = trackNumsCompareFrame(frameNumToProbe);
+        track = tracklistCompareFrame{trackNum};
+        [audioData, sampleRate] = audioread(track.filename);
+
+        % write the clip to file, clipSizeSeconds with probing frame in the
+        % middle
+        timeStart = frameTime - (clipSizeSeconds/2);
+        timeEnd = frameTime + (clipSizeSeconds/2);
+        idxStart = round(seconds(seconds(timeStart) * sampleRate));
+        idxEnd = round(seconds(seconds(timeEnd) * sampleRate));
+        newFilename = sprintf('%s\\clip%d-%dseconds.wav', clipDir, ...
+            frameNumToProbe, clipSizeSeconds);
+        clipData = audioData(idxStart:idxEnd);
+        audiowrite(newFilename, clipData, sampleRate);
+
+        fprintf(fileID, 'clip%d  timeSeconds=%.2f  filename=%s\n', ...
+            frameNumToProbe, frameTime, track.filename);
+        fprintf(fileID, '\tpredicted=%.2f  actual=%.2f\n', ...
+            yPred(frameNumToProbe), yCompareFrame(frameNumToProbe));
+        
+        numClipsCreated = numClipsCreated + 1;
+
+        % zero out the 
+        % check if any other frame number is within this frame's utterance
+        % monster frames are 10ms
+        clipSizeFrames = seconds(ignoreSizeSeconds) / milliseconds(10);
+        frameNumCompareStart = frameNumToProbe - clipSizeFrames / 2;
+        frameNumCompareEnd = frameNumToProbe + clipSizeFrames / 2;
+        
+        % adjust compare start and compare end if out of bounds
+        if frameNumCompareStart < 1
+            frameNumCompareStart = 1;
+        end
+        if frameNumCompareEnd > length(yDifference)
+            frameNumCompareEnd = length(yDifference);
+        end
+
+        for i = frameNumCompareStart:frameNumCompareEnd
+            % if this frame is in the same track and utterance as the 
+            % original, mark the frame to ignore it
+            if trackNumsCompareFrame(i) ~= ...
+                    trackNumsCompareFrame(frameNumToProbe)
+                continue;
+            end
+            if utterNumsCompareFrame(i) ~= ...
+                    utterNumsCompareFrame(frameNumToProbe)
+                continue;
+            end
+            framesToIgnore(i) = 1;
+        end
+    end
+    
+    fclose(fileID);
+    fprintf('Output written to %s\n', outputFilename);
+end
